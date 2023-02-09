175174167250bb91fb3fad155c4faad6ee3c6bcc
José Ithalo <jose_ithalo@hotmail.com>
2023-02-08 23:14:42 -0300

    Nova importação css/mobile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import './css/mobile.css';
 import MainRoutes from './routes';
 
 import { BrowserRouter } from 'react-router-dom';
