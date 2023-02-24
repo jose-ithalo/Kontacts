4ae4cfc6bcaf76339585a76d42e46de68f7cc5e2
José Ithalo <jose_ithalo@hotmail.com>
2023-02-23 23:27:36 -0300

    Máscara de telefone implementada ao input

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -2,11 +2,14 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import UserContext from '../../context/userContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 export default function BasicTextFields({ inputName, inputPassword, inputPhone }) {
 
     const { forms, setForms } = useContext(UserContext);
+
+    const [dddControl, setDddControl] = useState(false);
+    const [hyphen, setHyphen] = useState(false);
 
     function changeInputValue(evt) {
 
@@ -16,6 +19,58 @@ export default function BasicTextFields({ inputName, inputPassword, inputPhone }
         setForms({ ...forms, [inputName]: newValue });
 
     }
+
+    function inputValueTel(evt) {
+
+        const inputName = evt.target.name;
+        let newValue = evt.target.value;
+
+        if (newValue.length >= 16) {
+            return
+        }
+
+        if (newValue === '(' || newValue === ')') {
+            newValue = '';
+        } else if (newValue.length === 1) {
+            newValue = '(' + newValue;
+        }
+
+
+        if (newValue.length === 3) {
+
+            if (dddControl === false) {
+                newValue += ') '
+                setDddControl(!dddControl);
+            } else {
+
+                const listValue = newValue.split('');
+                listValue.pop();
+
+                newValue = listValue.join('');
+                setDddControl(!dddControl);
+
+            }
+        }
+
+        if (newValue.length === 10) {
+
+            if (hyphen === false) {
+                newValue += '-'
+                setHyphen(!hyphen);
+            } else {
+
+                const listValue = newValue.split('');
+                listValue.pop();
+
+                newValue = listValue.join('');
+                setHyphen(!hyphen);
+            }
+        }
+
+        setForms({ ...forms, [inputName]: newValue });
+
+    }
+
 
     return (
         <Box
@@ -42,8 +97,7 @@ export default function BasicTextFields({ inputName, inputPassword, inputPhone }
 
                 {inputPhone && <TextField id="outlined-basic" label="Telefone" variant="outlined"
                     type='tel' className='input-form' name='phone' value={forms.phone}
-                    onChange={(event) => changeInputValue(event)} />}
-
+                    onChange={(event) => inputValueTel(event)} />}
 
             </div>
 
