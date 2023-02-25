e653b5744ec7a4ce7ca15e4c3992cf7616547b80
José Ithalo <jose_ithalo@hotmail.com>
2023-02-24 21:50:41 -0300

    Filtragem de dados adicionado à tabela

--- src/components/tableMain/table.js
+++ src/components/tableMain/table.js
@@ -13,10 +13,15 @@ import trashIcon from '../../assets/trash.svg';
 import UserContext from '../../context/userContext';
 import { useContext } from 'react';
 
-export default function ContactsTable() {
+export default function ContactsTable({ search }) {
 
     const { contactList, setForms, setInfoContact, setStateModalDel,
         setStateModalUp, setShowLoading } = useContext(UserContext);
+
+    const filteredContacts = search.length > 0 ?
+        contactList.filter(function (data) {
+            return data.nome.toLowerCase().includes(search.toLowerCase());
+        }) : [];
 
     return (
         <TableContainer>
@@ -32,57 +37,111 @@ export default function ContactsTable() {
                 </TableHead>
                 <div style={{ marginTop: '30px' }}></div>
                 <TableBody>
-                    {contactList.map((data) => (
-                        <>
-                            <div style={{ marginBottom: '15px' }}></div>
-                            <TableRow className='table-row'
-                                key={data.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row" className="table-data">
-                                    {data.nome}
-                                </TableCell>
-                                <TableCell align="left" className="table-data">{data.email}</TableCell>
-                                <TableCell align="left" className="table-data">{data.telefone}</TableCell>
 
-                                <TableCell align="left">
-                                    <img src={editIcon} alt='Ícone editar' className='icon-table'
-                                        onClick={() => {
+                    {search.length === 0 ?
 
-                                            setForms({
-                                                name: data.nome,
-                                                email: data.email,
-                                                phone: data.telefone
-                                            });
+                        contactList.map((data) => (
+                            <>
+                                <div style={{ marginBottom: '15px' }}></div>
+                                <TableRow className='table-row'
+                                    key={data.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row" className="table-data">
+                                        {data.nome}
+                                    </TableCell>
+                                    <TableCell align="left" className="table-data">{data.email}</TableCell>
+                                    <TableCell align="left" className="table-data">{data.telefone}</TableCell>
 
-                                            setInfoContact(data.id);
+                                    <TableCell align="left">
+                                        <img src={editIcon} alt='Ícone editar' className='icon-table'
+                                            onClick={() => {
 
-                                            setStateModalUp(true);
+                                                setForms({
+                                                    name: data.nome,
+                                                    email: data.email,
+                                                    phone: data.telefone
+                                                });
 
-                                            setShowLoading(false);
-                                        }} />
-                                </TableCell>
+                                                setInfoContact(data.id);
 
-                                <TableCell align="left">
-                                    <img src={trashIcon} alt='lixeira' className='icon-table'
-                                        onClick={() => {
-                                            setInfoContact({
-                                                id: data.id,
-                                                nome: data.nome,
-                                                email: data.email,
-                                                telefone: data.telefone
-                                            });
+                                                setStateModalUp(true);
 
-                                            setStateModalDel(true);
+                                                setShowLoading(false);
+                                            }} />
+                                    </TableCell>
 
-                                            setShowLoading(false);
-                                        }} />
-                                </TableCell>
+                                    <TableCell align="left">
+                                        <img src={trashIcon} alt='lixeira' className='icon-table'
+                                            onClick={() => {
+                                                setInfoContact({
+                                                    id: data.id,
+                                                    nome: data.nome,
+                                                    email: data.email,
+                                                    telefone: data.telefone
+                                                });
 
-                            </TableRow>
-                        </>
+                                                setStateModalDel(true);
 
-                    ))}
+                                                setShowLoading(false);
+                                            }} />
+                                    </TableCell>
+
+                                </TableRow>
+                            </>
+
+                        )) :
+
+                        filteredContacts.map((data) => (
+                            <>
+                                <div style={{ marginBottom: '15px' }}></div>
+                                <TableRow className='table-row'
+                                    key={data.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row" className="table-data">
+                                        {data.nome}
+                                    </TableCell>
+                                    <TableCell align="left" className="table-data">{data.email}</TableCell>
+                                    <TableCell align="left" className="table-data">{data.telefone}</TableCell>
+
+                                    <TableCell align="left">
+                                        <img src={editIcon} alt='Ícone editar' className='icon-table'
+                                            onClick={() => {
+
+                                                setForms({
+                                                    name: data.nome,
+                                                    email: data.email,
+                                                    phone: data.telefone
+                                                });
+
+                                                setInfoContact(data.id);
+
+                                                setStateModalUp(true);
+
+                                                setShowLoading(false);
+                                            }} />
+                                    </TableCell>
+
+                                    <TableCell align="left">
+                                        <img src={trashIcon} alt='lixeira' className='icon-table'
+                                            onClick={() => {
+                                                setInfoContact({
+                                                    id: data.id,
+                                                    nome: data.nome,
+                                                    email: data.email,
+                                                    telefone: data.telefone
+                                                });
+
+                                                setStateModalDel(true);
+
+                                                setShowLoading(false);
+                                            }} />
+                                    </TableCell>
+
+                                </TableRow>
+                            </>
+                        ))}
                 </TableBody>
             </Table>
         </TableContainer>
